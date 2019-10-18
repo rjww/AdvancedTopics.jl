c1ee69b6ffbc84678fc7d4775ab975434cce342e
Robert Woods <git@robertwoods.me>
2019-10-18 11:41:58 +1030

    Add convenience functions for retrieving and manipulating MNIST data

--- src/AdvancedTopics.jl
+++ src/AdvancedTopics.jl
@@ -2,11 +2,15 @@ module AdvancedTopics
 
 import Distributions
 import KernelDensity
+import MLDatasets
+import Random
 
 include("kde_comparator.jl")
 include("kde_sieve_layer.jl")
 include("kde_sieve.jl")
+include("mnist_data.jl")
 
-export KDESieve, predict
+export KDESieve, predict,
+       training_data, testing_data
 
 end # module
---
+++ src/mnist_data.jl
@@ -0,0 +1,51 @@
+const filtering_labels = [0, 1]
+
+function training_data()
+    X, T = MLDatasets.MNIST.traindata()
+    reshape_data(filter_data(X, T)...)
+end
+
+function training_data(n_samples::Int)
+    random_sample(n_samples, training_data()...)
+end
+
+function testing_data()
+    X, T = MLDatasets.MNIST.testdata()
+    reshape_data(filter_data(X, T)...)
+end
+
+function testing_data(n_samples::Int)
+    random_sample(n_samples, testing_data()...)
+end
+
+function filter_data(samples::T1,
+                     targets::T2) where {T1 <: AbstractArray,
+                                         T2 <: AbstractVector}
+    indices = findall(x -> x ∈ filtering_labels, targets)
+    (samples[:,:,indices], targets[indices])
+end
+
+function reshape_data(samples::T1,
+                      targets::T2) where {T1 <: AbstractArray,
+                                          T2 <: AbstractVector}
+    (reshape_samples(samples), reshape_targets(targets))
+end
+
+function reshape_samples(samples::T) where {T <: AbstractArray}
+    rows, cols, N = size(samples)
+    reshape(samples, (rows * cols, N))
+end
+
+function reshape_targets(targets::T) where {T <: AbstractVector}
+    [x == 0 ? -1 : 1 for x ∈ targets]
+end
+
+function random_sample(n_samples::Int,
+                       samples::T1,
+                       targets::T2) where {T1 <: AbstractArray,
+                                           T2 <: AbstractVector}
+    @assert n_samples <= length(targets) "Blah."
+    range = length(targets)
+    indices = Random.randperm(range)[1:n_samples]
+    (samples[:,indices], targets[indices])
+end
